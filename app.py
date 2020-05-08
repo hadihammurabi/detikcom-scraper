98745131b2927742a5685c65b2a11d7a0cb759d6
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-05-09 01:23:43 +0700

    export after get html source

--- app.py
+++ app.py
@@ -7,4 +7,7 @@ options.headless = True
 web = webdriver.Firefox(options=options)
 web.get('https://news.detik.com/berita/d-5007699/influencer-ini-bagikan-tips-tetap-produktif-saat-pandemi-covid-19')
 html = web.page_source
+export = open("berita/d-5007699_influencer-ini-bagikan-tips-tetap-produktif-saat-pandemi-covid-19.html", "a")
+export.write(html)
+export.close()
 web.quit()
