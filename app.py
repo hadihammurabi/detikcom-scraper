7e47e8c2af11d78447cf484624b9d2791330f62f
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-05-09 09:54:36 +0700

    parsing and cleaning

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+# Firefox setting
 options = webdriver.FirefoxOptions()
 options.headless = True
 
+# Website opening
 web = webdriver.Firefox(options=options)
 web.get('https://news.detik.com/berita/d-5007699/influencer-ini-bagikan-tips-tetap-produktif-saat-pandemi-covid-19')
 html = web.page_source
-export = open("berita/d-5007699_influencer-ini-bagikan-tips-tetap-produktif-saat-pandemi-covid-19.html", "a")
-export.write(html)
-export.close()
 web.quit()
+
+# Website document processing
+doc = BeautifulSoup(html, features='lxml')
+
+# Get information/data
+title = doc.find('h1', { 'class': 'detail__title' }).getText()
+contents = doc.find('div', { 'class': 'detail__body-text' }).findAll('p')
+
+# Clean and save
+data = {}
+data['title'] = title.replace('\n', '').strip()
+data['body'] = ''
+for content in contents:
+  data['body'] += content.getText()
+
+print(data)
