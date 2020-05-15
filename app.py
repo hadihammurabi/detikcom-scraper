d78592c971a9a7413190d49f4ce270ca793d6565
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-05-15 22:57:47 +0700

    wrap to run fn

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import scraper
 from parser import detikcom
 
-url = 'https://news.detik.com/berita/d-5007699/influencer-ini-bagikan-tips-tetap-produktif-saat-pandemi-covid-19'
+def run():
+  url = 'https://news.detik.com/berita/d-5007699/influencer-ini-bagikan-tips-tetap-produktif-saat-pandemi-covid-19'
 
-html = ''
-if detikcom.isexists(url):
-  html = detikcom.get(url)
-else:
-  html = scraper.get(url)
+  html = ''
+  if detikcom.isexists(url):
+    html = detikcom.get(url)
+  else:
+    html = scraper.get(url)
 
-data = detikcom.parse(html)
-print(data)
+  data = detikcom.parse(html)
+  print(data)
+
+if __name__ == '__main__':
+  run()
