e85ea13aaf3b6d355fd2b520feca83d024e91475
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-05-16 00:07:17 +0700

    use chrome driver

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 
 def get_index(**opts):
-  options = webdriver.FirefoxOptions()
+  options = webdriver.ChromeOptions()
   options.headless = True
 
   # Website opening
-  web = webdriver.Firefox(options=options)
+  web = webdriver.Chrome(options=options)
 
   if not opts['page'] or opts['page'] < 1:
     opts['page'] = 1
@@ -20,12 +20,12 @@ def get_index(**opts):
   return html
 
 def get_berita(url):
-  # Firefox setting
-  options = webdriver.FirefoxOptions()
+  # Chrome setting
+  options = webdriver.ChromeOptions()
   options.headless = True
 
   # Website opening
-  web = webdriver.Firefox(options=options)
+  web = webdriver.Chrome(options=options)
   web.get(url)
   html = web.page_source
   web.quit()
