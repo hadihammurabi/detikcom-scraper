f85338621fc2c8e47d90849b1208cf1e06c7c29d
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-05-15 23:26:06 +0700

    add news index scraper

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
 
-def get(url):
+def get_index(**opts):
+  options = webdriver.FirefoxOptions()
+  options.headless = True
+
+  # Website opening
+  web = webdriver.Firefox(options=options)
+
+  if not opts['page'] or opts['page'] < 1:
+    opts['page'] = 1
+  
+  if opts['page'] and opts['page'] > 24:
+    opts['page'] = 24
+
+  web.get("https://news.detik.com/indeks/{}?date={}".format(opts['page'], opts['date']))
+  html = web.page_source
+  web.quit()
+
+  return html
+
+def get_berita(url):
   # Firefox setting
   options = webdriver.FirefoxOptions()
   options.headless = True
