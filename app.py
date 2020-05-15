fafc064328cc4d9dc2d2724f70b8cbaedc034f19
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-05-16 00:00:44 +0700

    add bundled fns

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import scraper
 import parser.detikcom as parser
 
+def get_and_parse_index(**opts):
+  html_index = scraper.get_index(date=opts['date'], page=opts['page'])
+  parsed_index = parser.parse_index(html_index)
+  return parsed_index
+
+def get_and_parse_berita(url):
+  html_berita = scraper.get_berita(url)
+  parsed_berita = parser.parse_berita(html_berita)
+  return parsed_berita
+
 def run():
-  html_index = scraper.get_index(date="05/15/2020", page=1)
-  indexes = parser.parse_index(html_index)
+  indexes = get_and_parse_index(date="05/15/2020", page=1)
   print(indexes)
 
   with open('output', 'a') as f:
@@ -11,8 +20,7 @@ def run():
     f.close()
 
   for index in indexes:
-    html_berita = scraper.get_berita(index['url'])
-    data = parser.parse_berita(html_berita)
+    data = get_and_parse_berita(index['url'])
     print(data)
     with open('output', 'a') as f:
       f.write(str(data))
